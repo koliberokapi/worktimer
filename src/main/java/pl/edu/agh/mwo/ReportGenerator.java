8a765b5ecb5af42c621d2e9071815fbc35076df0
Elzbieta <elzbieta_warzecha@wp.pl>
2023-05-28 14:33:32 +0200

    #28 wypisywanie raportu

--- src/main/java/pl/edu/agh/mwo/ReportGenerator.java
+++ src/main/java/pl/edu/agh/mwo/ReportGenerator.java
@@ -6,15 +6,16 @@ public class ReportGenerator {
 
     public void displayReport(ReportCalc reportCalc) {
         HashMap<String, HashMap<String, Long>> data = reportCalc.calculateReport();
-        System.out.printf("%-20s %-20s %10s", "NAZWA PROJEKTU", "NAZWA TASKA", "CZAS TRWANIA");
+        System.out.printf("%-20s %-20s %20s", "NAZWA PROJEKTU", "NAZWA TASKA", "CZAS TRWANIA");
         System.out.println();
         for(String m : data.keySet()){
-            System.out.println("--------------------------------------------------------");
-            System.out.println(m);
-            System.out.println("--------------------------------------------------------");
+            System.out.println("------------------------------------------------------------------");
+            System.out.printf("%-20s %-20s %20s", m, " ", reportCalc.getDurationBreakdown(reportCalc.sumPerProject(m)));
+            System.out.println();
+            System.out.println("------------------------------------------------------------------");
             for(String taskName: data.get(m).keySet()) {
                 Long taskDuration = data.get(m).get(taskName);
-                System.out.printf("%-20s %-20s %10s", "", taskName, reportCalc.getDurationBreakdown(taskDuration));
+                System.out.printf("%-20s %-20s %20s", " ", taskName, ReportCalc.getDurationBreakdown(taskDuration));
                 System.out.println();
             }
 
